fb3caa2c007b525f6062f368061817fbadfc0c87
othmane <othman-19@hotmail.fr>
2020-06-10 23:03:27 +0100

    Solve the problem: Maximum Depth of Binary Tree

---
+++ Trees/Maximum_Depth_of_Binary_Tree.js
@@ -0,0 +1,20 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxDepth = function(root) {
+  if (!root) return 0;
+  let left_depth = maxDepth(root.left);
+  let right_depth = maxDepth(root.right);
+  return Math.max(left_depth, right_depth) + 1 
+};
+
+maxDepth()
